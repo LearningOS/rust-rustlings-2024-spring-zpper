d5c1c69c63b030d2db3cd49dca3d75f4af83ab71
fffnfg <fffnfg@foxmail>
2024-04-08 13:33:14 +0800

    update

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+use std::str::Chars;
 
 fn main() {
-    let a = ???
+    let a: Vec<Chars> = Vec::new();
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
