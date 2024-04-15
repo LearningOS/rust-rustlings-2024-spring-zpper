ec8991e90f450aacd9882a878c9abdc82d16d54d
fffnfg <fffnfg@foxmail>
2024-04-15 23:29:52 +0800

    update

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,15 +3,15 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
+use std::ptr::NonNull;
 
 
 #[derive(Debug)]
 struct TreeNode<T>
-where
-    T: Ord,
+    where
+        T: Ord,
 {
     value: T,
     left: Option<Box<TreeNode<T>>>,
@@ -20,15 +20,15 @@ where
 
 #[derive(Debug)]
 struct BinarySearchTree<T>
-where
-    T: Ord,
+    where
+        T: Ord + Copy,
 {
     root: Option<Box<TreeNode<T>>>,
 }
 
 impl<T> TreeNode<T>
-where
-    T: Ord,
+    where
+        T: Ord,
 {
     fn new(value: T) -> Self {
         TreeNode {
@@ -40,33 +40,78 @@ where
 }
 
 impl<T> BinarySearchTree<T>
-where
-    T: Ord,
+    where
+        T: Ord + Copy,
 {
-
     fn new() -> Self {
         BinarySearchTree { root: None }
     }
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        if let Some(ref mut node) = self.root {
+            node.insert(value);
+        } else {
+            self.root = Some(Box::new(TreeNode::new(value)));
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        if let Some(ref node) = self.root {
+            return node.search(value);
+        }
+        false
     }
 }
 
-impl<T> TreeNode<T>
-where
-    T: Ord,
+impl<T> TreeNode<T> where T: Ord + Clone,
 {
     // Insert a node into the tree
-    fn insert(&mut self, value: T) {
-        //TODO
+    fn insert(&mut self, value: T) where T: Ord + Copy {
+        match value {
+            n if n < self.value => {
+                match self.left {
+                    Some(ref mut left_node) => {
+                        left_node.insert(value);
+                    }
+                    None => {
+                        self.left = Some(Box::new(TreeNode::new(value)));
+                    }
+                }
+            }
+            n if n > self.value => {
+                match self.right {
+                    Some(ref mut right_node) => {
+                        right_node.insert(value);
+                    }
+                    None => {
+                        self.right = Some(Box::new(TreeNode::new(value)));
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn search(&self, value: T) -> bool where T: Ord + Copy {
+        if self.value == value {
+            return true;
+        }
+
+        if let Some(ref left) = self.left {
+            if left.search(value) {
+                return true;
+            }
+        }
+
+        if let Some(ref right) = self.right {
+            if right.search(value) {
+                return true;
+            }
+        }
+
+        false
     }
 }
 
@@ -79,24 +124,24 @@ mod tests {
     fn test_insert_and_search() {
         let mut bst = BinarySearchTree::new();
 
-        
+
         assert_eq!(bst.search(1), false);
 
-        
+
         bst.insert(5);
         bst.insert(3);
         bst.insert(7);
         bst.insert(2);
         bst.insert(4);
 
-        
+
         assert_eq!(bst.search(5), true);
         assert_eq!(bst.search(3), true);
         assert_eq!(bst.search(7), true);
         assert_eq!(bst.search(2), true);
         assert_eq!(bst.search(4), true);
 
-        
+
         assert_eq!(bst.search(1), false);
         assert_eq!(bst.search(6), false);
     }
@@ -105,19 +150,19 @@ mod tests {
     fn test_insert_duplicate() {
         let mut bst = BinarySearchTree::new();
 
-        
+
         bst.insert(1);
         bst.insert(1);
 
-        
+
         assert_eq!(bst.search(1), true);
 
-        
+
         match bst.root {
             Some(ref node) => {
                 assert!(node.left.is_none());
                 assert!(node.right.is_none());
-            },
+            }
             None => panic!("Root should not be None after insertion"),
         }
     }
